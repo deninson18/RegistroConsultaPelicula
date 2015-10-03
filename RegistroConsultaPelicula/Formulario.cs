7ebb4f1369c86fe7d976942c6a401ba4ff8364db
deninson18 <deninson050895@gmail.com>
2015-10-03 12:00:04 -0430

    agregando registro y consulta de peliculas

---
+++ RegistroConsultaPelicula/Formulario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroConsultaPelicula
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void registroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RegistroPelicula r = new RegistroPelicula();
+            r.Show();
+        }
+
+        private void consultaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Consulta c = new Consulta();
+            c.Show();
+        }
+    }
+}
